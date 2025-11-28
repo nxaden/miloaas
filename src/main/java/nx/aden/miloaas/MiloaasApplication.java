0742b12e1e162f9ead3d86e8bc480ab106d824e4
developingbymoe <muhammaddddali1@gmail.com>
2025-11-28 03:38:17 -0500

    Initial commit

---
+++ src/main/java/nx/aden/miloaas/MiloaasApplication.java
@@ -0,0 +1,13 @@
+package nx.aden.miloaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiloaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiloaasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nx/aden/miloaas/MiloaasApplicationTests.java
@@ -0,0 +1,13 @@
+package nx.aden.miloaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiloaasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
